fe18e747ea66cefaa34f9faf61610a1b51c8458d
w-mb <william.baines@students.plymouth.ac.uk>
2022-01-18 17:03:35 +0000

    Removed efficiency from superpick

--- RuneCraftItems/src/com/inceris/runecraftitems/ItemList.java
+++ RuneCraftItems/src/com/inceris/runecraftitems/ItemList.java
@@ -14,7 +14,7 @@ public class ItemList {
 	public static ItemStack superpick() {
 		ItemStack item = new ItemStack(Material.NETHERITE_PICKAXE);
 		item.addUnsafeEnchantment(Enchantment.ARROW_INFINITE, 10);
-		item.addEnchantment(Enchantment.DIG_SPEED, 5);
+//		item.addEnchantment(Enchantment.DIG_SPEED, 5);
 		Damageable meta = (Damageable) item.getItemMeta();
 		meta.setDamage(2031 - rci.getConfig().getInt("items.superpick.durability"));
 		meta.setDisplayName(ChatColor.translateAlternateColorCodes('&', rci.getConfig().getString("items.superpick.name")));
