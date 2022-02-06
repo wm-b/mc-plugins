4dc49a6a40a2fc116b035a5fd5bfdae2c6c8023a
w-mb <william.baines@students.plymouth.ac.uk>
2022-02-06 03:57:35 +0000

    Fixed bugs with enchants not applying properly and reverted item preview without permission.

--- ATSUtilities/src/com/inceris/atsutilities/listeners/PrepareAnvilListener.java
+++ ATSUtilities/src/com/inceris/atsutilities/listeners/PrepareAnvilListener.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.HumanEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.PrepareAnvilEvent;
@@ -24,13 +25,33 @@ public class PrepareAnvilListener implements Listener {
 		return meta.getStoredEnchants();
 	}
 	
+	private ItemStack putEnchantmentOnBook(ItemStack item, Enchantment enchantment, int level) {
+		EnchantmentStorageMeta meta = (EnchantmentStorageMeta) item.getItemMeta();
+		meta.addStoredEnchant(enchantment, level, true);
+		item.setItemMeta(meta);
+		return item;
+	}
+	
+	private boolean allowedPlayerViewing(List<HumanEntity> viewers, Enchantment enchantment, int level) {
+		Player p = (Player) viewers.get(0);
+		int prestigeLevelRequired = level - enchantment.getMaxLevel();
+		if (prestigeLevelRequired > 0) {
+			if (p.hasPermission("atsutilities.prestige." + prestigeLevelRequired)) {
+				return true;
+			}
+		} else {
+			return true;
+		}
+		return false;
+	}
+	
 	private void generateResult(Enchantment enchantment, ItemStack base, ItemStack addition, ItemStack result, List<HumanEntity> viewers) {
 		
 		Map<Enchantment, Integer> baseEnchants = null;
 		Map<Enchantment, Integer> additionEnchants = null;
 
 		Material baseType = base.getType();
-		if (base.getType().equals(Material.ENCHANTED_BOOK)) {
+		if (baseType.equals(Material.ENCHANTED_BOOK)) {
 			baseEnchants = enchantedBook(base);
 		} else {
 			baseEnchants = base.getEnchantments();
@@ -44,28 +65,58 @@ public class PrepareAnvilListener implements Listener {
 		}
 
 		if (baseEnchants.containsKey(enchantment)
-				&& additionEnchants.containsKey(enchantment)) {
+				|| additionEnchants.containsKey(enchantment)) {
 
-			if (baseEnchants.get(enchantment).equals(additionEnchants.get(enchantment))) {
-
-				if (atsu.debug)
-					atsu.getLogger().info("Identified " + enchantment + " craft");
-
-				if (result.getType().equals(Material.ENCHANTED_BOOK)) {
-					EnchantmentStorageMeta meta = (EnchantmentStorageMeta) result.getItemMeta();
-					meta.addStoredEnchant(enchantment, baseEnchants.get(enchantment) + 1, true);
-					result.setItemMeta(meta);
+			int additionEnchantLevel = 0;
+			if (additionEnchants.get(enchantment) != null) {
+				additionEnchantLevel = additionEnchants.get(enchantment);
+			}
+			
+			int baseEnchantLevel = 0;
+			if (baseEnchants.get(enchantment) != null) {
+				baseEnchantLevel = baseEnchants.get(enchantment);
+			}
+			
+			if (baseEnchantLevel == additionEnchantLevel) {
+				
+				int resultLevel = baseEnchantLevel + 1;
+				if (allowedPlayerViewing(viewers, enchantment, resultLevel)) {
+					if (result.getType().equals(Material.ENCHANTED_BOOK)) {
+						putEnchantmentOnBook(result, enchantment, resultLevel);
+					} else {
+						result.addUnsafeEnchantment(enchantment, resultLevel);
+					}
 				} else {
-					result.addUnsafeEnchantment(enchantment, baseEnchants.get(enchantment) + 1);
+					if (result.getType().equals(Material.ENCHANTED_BOOK)) {
+						putEnchantmentOnBook(result, enchantment, baseEnchantLevel);
+					} else {
+						result.addUnsafeEnchantment(enchantment, baseEnchantLevel);
+					}
 				}
-			}
 
-		} else if (!baseType.equals(Material.ENCHANTED_BOOK) && additionType.equals(Material.ENCHANTED_BOOK)) {
-			for (Map.Entry<Enchantment, Integer> e : additionEnchants.entrySet()) {
-				if (e.getKey().canEnchantItem(base)) {
-					result.addUnsafeEnchantment(e.getKey(), e.getValue());
+			} else if (baseEnchantLevel < additionEnchantLevel) {
+
+				if (allowedPlayerViewing(viewers, enchantment, additionEnchantLevel)) {
+					if (result.getType().equals(Material.ENCHANTED_BOOK)) {
+						putEnchantmentOnBook(result, enchantment, additionEnchantLevel);
+					} else {
+						result.addUnsafeEnchantment(enchantment, additionEnchantLevel);
+					}
+				} else {
+					if (result.getType().equals(Material.ENCHANTED_BOOK)) {
+						putEnchantmentOnBook(result, enchantment, baseEnchantLevel);
+					} else {
+						result.addUnsafeEnchantment(enchantment, baseEnchantLevel);
+					}
 				}
-			}
+			} else {
+					if (result.getType().equals(Material.ENCHANTED_BOOK)) {
+						putEnchantmentOnBook(result, enchantment, baseEnchantLevel);
+					} else {
+						result.addUnsafeEnchantment(enchantment, baseEnchantLevel);
+					}
+				}
+
 		}
 	}
 
