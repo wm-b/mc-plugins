3a743464223334acbf990bab1a39eb868e238d3e
w-mb <william.baines@students.plymouth.ac.uk>
2022-02-25 00:21:29 +0000

    Added a null check.

--- Lockout/src/com/inceris/lockout/listeners/InventoryClickListener.java
+++ Lockout/src/com/inceris/lockout/listeners/InventoryClickListener.java
@@ -61,57 +61,60 @@ public class InventoryClickListener implements Listener {
 					Util.checkWearingArmorSet(gi, p, objectives, Objective.wearDiamondSet, ac, Material.DIAMOND_HELMET,
 							Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS);
 
-					Material m = e.getCurrentItem().getType();
-					if (m.equals(Material.DIAMOND) && objectives.contains(Objective.obtainDiamond)) {
-						Objective.complete(gi, Objective.obtainDiamond, p);
+					if (e.getCurrentItem() != null) {
+						Material m = e.getCurrentItem().getType();
+						if (m.equals(Material.DIAMOND) && objectives.contains(Objective.obtainDiamond)) {
+							Objective.complete(gi, Objective.obtainDiamond, p);
 
-					} else if (m.equals(Material.REDSTONE) && objectives.contains(Objective.obtainRedstone)) {
-						Objective.complete(gi, Objective.obtainRedstone, p);
+						} else if (m.equals(Material.REDSTONE) && objectives.contains(Objective.obtainRedstone)) {
+							Objective.complete(gi, Objective.obtainRedstone, p);
 
-					} else if (m.equals(Material.LAPIS_LAZULI) && objectives.contains(Objective.obtainLapis)) {
-						Objective.complete(gi, Objective.obtainLapis, p);
+						} else if (m.equals(Material.LAPIS_LAZULI) && objectives.contains(Objective.obtainLapis)) {
+							Objective.complete(gi, Objective.obtainLapis, p);
 
-					} else if (m.equals(Material.COAL) && objectives.contains(Objective.obtainCoal)) {
-						Objective.complete(gi, Objective.obtainCoal, p);
+						} else if (m.equals(Material.COAL) && objectives.contains(Objective.obtainCoal)) {
+							Objective.complete(gi, Objective.obtainCoal, p);
 
-					} else if (m.equals(Material.RAW_IRON) && objectives.contains(Objective.obtainRawIron)) {
-						Objective.complete(gi, Objective.obtainRawIron, p);
+						} else if (m.equals(Material.RAW_IRON) && objectives.contains(Objective.obtainRawIron)) {
+							Objective.complete(gi, Objective.obtainRawIron, p);
 
-					} else if (m.equals(Material.RAW_GOLD) && objectives.contains(Objective.obtainRawGold)) {
-						Objective.complete(gi, Objective.obtainRawGold, p);
+						} else if (m.equals(Material.RAW_GOLD) && objectives.contains(Objective.obtainRawGold)) {
+							Objective.complete(gi, Objective.obtainRawGold, p);
 
-					} else if (m.equals(Material.IRON_INGOT) && objectives.contains(Objective.obtainIronIngot)) {
-						Objective.complete(gi, Objective.obtainIronIngot, p);
+						} else if (m.equals(Material.IRON_INGOT) && objectives.contains(Objective.obtainIronIngot)) {
+							Objective.complete(gi, Objective.obtainIronIngot, p);
 
-					} else if (m.equals(Material.GOLD_INGOT) && objectives.contains(Objective.obtainGoldIngot)) {
-						Objective.complete(gi, Objective.obtainGoldIngot, p);
+						} else if (m.equals(Material.GOLD_INGOT) && objectives.contains(Objective.obtainGoldIngot)) {
+							Objective.complete(gi, Objective.obtainGoldIngot, p);
 
-					} else if (m.equals(Material.TUFF) && objectives.contains(Objective.obtainTuff)) {
-						Objective.complete(gi, Objective.obtainTuff, p);
+						} else if (m.equals(Material.TUFF) && objectives.contains(Objective.obtainTuff)) {
+							Objective.complete(gi, Objective.obtainTuff, p);
 
-					} else if (m.equals(Material.GRANITE) && objectives.contains(Objective.obtainGranite)) {
-						Objective.complete(gi, Objective.obtainGranite, p);
+						} else if (m.equals(Material.GRANITE) && objectives.contains(Objective.obtainGranite)) {
+							Objective.complete(gi, Objective.obtainGranite, p);
 
-					} else if (m.equals(Material.ANDESITE) && objectives.contains(Objective.obtainAndesite)) {
-						Objective.complete(gi, Objective.obtainAndesite, p);
+						} else if (m.equals(Material.ANDESITE) && objectives.contains(Objective.obtainAndesite)) {
+							Objective.complete(gi, Objective.obtainAndesite, p);
 
-					} else if (m.equals(Material.DIORITE) && objectives.contains(Objective.obtainDiorite)) {
-						Objective.complete(gi, Objective.obtainDiorite, p);
+						} else if (m.equals(Material.DIORITE) && objectives.contains(Objective.obtainDiorite)) {
+							Objective.complete(gi, Objective.obtainDiorite, p);
 
-					} else if (m.equals(Material.COBBLED_DEEPSLATE) && objectives.contains(Objective.obtainDeepslate)) {
-						Objective.complete(gi, Objective.obtainDeepslate, p);
+						} else if (m.equals(Material.COBBLED_DEEPSLATE)
+								&& objectives.contains(Objective.obtainDeepslate)) {
+							Objective.complete(gi, Objective.obtainDeepslate, p);
 
-					} else if (m.equals(Material.ANCIENT_DEBRIS)
-							&& objectives.contains(Objective.obtainAncientDebris)) {
-						Objective.complete(gi, Objective.obtainAncientDebris, p);
+						} else if (m.equals(Material.ANCIENT_DEBRIS)
+								&& objectives.contains(Objective.obtainAncientDebris)) {
+							Objective.complete(gi, Objective.obtainAncientDebris, p);
 
-					} else if (m.equals(Material.NETHERITE_INGOT)
-							&& objectives.contains(Objective.obtainNetheriteIngot)) {
-						Objective.complete(gi, Objective.obtainNetheriteIngot, p);
+						} else if (m.equals(Material.NETHERITE_INGOT)
+								&& objectives.contains(Objective.obtainNetheriteIngot)) {
+							Objective.complete(gi, Objective.obtainNetheriteIngot, p);
 
-					} else if (m.equals(Material.BEACON) && objectives.contains(Objective.obtainBeacon)) {
-						Objective.complete(gi, Objective.obtainBeacon, p);
+						} else if (m.equals(Material.BEACON) && objectives.contains(Objective.obtainBeacon)) {
+							Objective.complete(gi, Objective.obtainBeacon, p);
 
+						}
 					}
 				}
 			}
