0c35c10663ae63bd89b6449de501320046ea796e
w-mb <william.baines@students.plymouth.ac.uk>
2022-03-01 01:00:39 +0000

    Fixed a bug where most "find" objectives weren't triggering.

--- Lockout/src/com/inceris/lockout/listeners/PlayerMoveListener.java
+++ Lockout/src/com/inceris/lockout/listeners/PlayerMoveListener.java
@@ -19,12 +19,13 @@ import com.inceris.lockout.util.Util;
 public class PlayerMoveListener implements Listener {
 
 	public static Lockout pl = Lockout.getPlugin(Lockout.class);
-	public static int actuallyCheck = 0;
+	public static int actuallyCheck = 1;
 
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent e) {
-		if (actuallyCheck == 20) {
-			actuallyCheck = 0;
+
+		if (actuallyCheck > 20) {
+			actuallyCheck = 1;
 		}
 
 		if (actuallyCheck == 10 || actuallyCheck == 20) {
@@ -43,87 +44,88 @@ public class PlayerMoveListener implements Listener {
 					} else if (objectives.contains(Objective.reachHeightLimit) && l.getY() >= 320) {
 						Objective.complete(gi, Objective.reachHeightLimit, p);
 
-					} else if (objectives.contains(Objective.reachHeightLimit) && l.getY() >= 320) {
-						Objective.complete(gi, Objective.reachHeightLimit, p);
+					}
 
-					} else if (objectives.contains(Objective.findMineshaft)
-							&& w.locateNearestStructure(l, StructureType.MINESHAFT, 50, false) != null
-							&& w.locateNearestStructure(l, StructureType.MINESHAFT, 50, false).getWorld()
+					l.setY(l.getY() - 50);
+					if (objectives.contains(Objective.findMineshaft)
+							&& w.locateNearestStructure(l, StructureType.MINESHAFT, 4, false) != null
+							&& w.locateNearestStructure(l, StructureType.MINESHAFT, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& w.locateNearestStructure(l, StructureType.MINESHAFT, 50, false).distance(l) < 50) {
+							&& w.locateNearestStructure(l, StructureType.MINESHAFT, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findMineshaft, p);
 
 					} else if (objectives.contains(Objective.findPyramid)
-							&& w.locateNearestStructure(l, StructureType.DESERT_PYRAMID, 50, false) != null
-							&& w.locateNearestStructure(l, StructureType.DESERT_PYRAMID, 50, false).getWorld()
+							&& w.locateNearestStructure(l, StructureType.DESERT_PYRAMID, 4, false) != null
+							&& w.locateNearestStructure(l, StructureType.DESERT_PYRAMID, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& w.locateNearestStructure(l, StructureType.DESERT_PYRAMID, 50, false).distance(l) < 50) {
+							&& w.locateNearestStructure(l, StructureType.DESERT_PYRAMID, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findPyramid, p);
 
 					} else if (objectives.contains(Objective.findVillage)
-							&& w.locateNearestStructure(l, StructureType.VILLAGE, 50, false) != null
-							&& w.locateNearestStructure(l, StructureType.VILLAGE, 50, false).getWorld()
+							&& w.locateNearestStructure(l, StructureType.VILLAGE, 4, false) != null
+							&& w.locateNearestStructure(l, StructureType.VILLAGE, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& w.locateNearestStructure(l, StructureType.VILLAGE, 50, false).distance(l) < 50) {
+							&& w.locateNearestStructure(l, StructureType.VILLAGE, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findVillage, p);
 
 					} else if (objectives.contains(Objective.findRuinedPortal)
-							&& w.locateNearestStructure(l, StructureType.RUINED_PORTAL, 50, false) != null
-							&& w.locateNearestStructure(l, StructureType.RUINED_PORTAL, 50, false).getWorld()
+							&& w.locateNearestStructure(l, StructureType.RUINED_PORTAL, 4, false) != null
+							&& w.locateNearestStructure(l, StructureType.RUINED_PORTAL, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& w.locateNearestStructure(l, StructureType.RUINED_PORTAL, 50, false).distance(l) < 50) {
+							&& w.locateNearestStructure(l, StructureType.RUINED_PORTAL, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findRuinedPortal, p);
 
 					} else if (objectives.contains(Objective.findOceanRuins)
-							&& w.locateNearestStructure(l, StructureType.OCEAN_RUIN, 50, false) != null
-							&& w.locateNearestStructure(l, StructureType.OCEAN_RUIN, 50, false).getWorld()
+							&& w.locateNearestStructure(l, StructureType.OCEAN_RUIN, 4, false) != null
+							&& w.locateNearestStructure(l, StructureType.OCEAN_RUIN, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& w.locateNearestStructure(l, StructureType.OCEAN_RUIN, 50, false).distance(l) < 50) {
+							&& w.locateNearestStructure(l, StructureType.OCEAN_RUIN, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findOceanRuins, p);
 
 					} else if (objectives.contains(Objective.findShipwreck)
-							&& w.locateNearestStructure(l, StructureType.SHIPWRECK, 50, false) != null
-							&& w.locateNearestStructure(l, StructureType.SHIPWRECK, 50, false).getWorld()
+							&& w.locateNearestStructure(l, StructureType.SHIPWRECK, 4, false) != null
+							&& w.locateNearestStructure(l, StructureType.SHIPWRECK, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& w.locateNearestStructure(l, StructureType.SHIPWRECK, 50, false).distance(l) < 50) {
+							&& w.locateNearestStructure(l, StructureType.SHIPWRECK, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findShipwreck, p);
 
 					} else if (objectives.contains(Objective.findFortress)
-							&& n.locateNearestStructure(l, StructureType.NETHER_FORTRESS, 50, false) != null
-							&& n.locateNearestStructure(l, StructureType.NETHER_FORTRESS, 50, false).getWorld()
+							&& n.locateNearestStructure(l, StructureType.NETHER_FORTRESS, 4, false) != null
+							&& n.locateNearestStructure(l, StructureType.NETHER_FORTRESS, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& n.locateNearestStructure(l, StructureType.NETHER_FORTRESS, 50, false).distance(l) < 50) {
+							&& n.locateNearestStructure(l, StructureType.NETHER_FORTRESS, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findFortress, p);
 
 					} else if (objectives.contains(Objective.findBastion)
-							&& n.locateNearestStructure(l, StructureType.BASTION_REMNANT, 50, false) != null
-							&& n.locateNearestStructure(l, StructureType.BASTION_REMNANT, 50, false).getWorld()
+							&& n.locateNearestStructure(l, StructureType.BASTION_REMNANT, 4, false) != null
+							&& n.locateNearestStructure(l, StructureType.BASTION_REMNANT, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& n.locateNearestStructure(l, StructureType.BASTION_REMNANT, 50, false).distance(l) < 50) {
+							&& n.locateNearestStructure(l, StructureType.BASTION_REMNANT, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findBastion, p);
 
 					} else if (objectives.contains(Objective.findNetherFossil)
-							&& n.locateNearestStructure(l, StructureType.NETHER_FOSSIL, 50, false) != null
-							&& n.locateNearestStructure(l, StructureType.NETHER_FOSSIL, 50, false).getWorld()
+							&& n.locateNearestStructure(l, StructureType.NETHER_FOSSIL, 4, false) != null
+							&& n.locateNearestStructure(l, StructureType.NETHER_FOSSIL, 4, false).getWorld()
 									.equals(l.getWorld())
-							&& n.locateNearestStructure(l, StructureType.NETHER_FOSSIL, 50, false).distance(l) < 50) {
+							&& n.locateNearestStructure(l, StructureType.NETHER_FOSSIL, 4, false).distanceSquared(l) < 2500) {
 						Objective.complete(gi, Objective.findNetherFossil, p);
 
 					}
 				}
 			}
+		}
 
-			if (actuallyCheck == 4 || actuallyCheck == 8 || actuallyCheck == 12 || actuallyCheck == 16
-					|| actuallyCheck == 20) {
-				if (GameInstance.get(p) != null) {
-					GameInstance gi = GameInstance.get(p);
-					Bukkit.getScheduler().runTaskAsynchronously(pl, new Runnable() {
-						@Override
-						public void run() {
-							Util.refreshCompass(gi, p);
-						}
-					});
-				}
+		if (actuallyCheck == 4 || actuallyCheck == 8 || actuallyCheck == 12 || actuallyCheck == 16
+				|| actuallyCheck == 20) {
+			Player p = e.getPlayer();
+			if (GameInstance.get(p) != null) {
+				GameInstance gi = GameInstance.get(p);
+				Bukkit.getScheduler().runTaskAsynchronously(pl, new Runnable() {
+					@Override
+					public void run() {
+						Util.refreshCompass(gi, p);
+					}
+				});
 			}
 		}
 
