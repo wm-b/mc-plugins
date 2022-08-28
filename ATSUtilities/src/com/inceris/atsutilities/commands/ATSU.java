63c47668b3795552fd5a453b76ed4a7f847df7c1
w-mb <william.baines@students.plymouth.ac.uk>
2022-08-29 00:43:34 +0100

    Added command to turn people into bats.

--- ATSUtilities/src/com/inceris/atsutilities/commands/ATSU.java
+++ ATSUtilities/src/com/inceris/atsutilities/commands/ATSU.java
@@ -2,7 +2,11 @@ package com.inceris.atsutilities.commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.GameMode;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -29,6 +33,41 @@ public class ATSU {
 		} else if (args[0].equalsIgnoreCase("debug") && sender.hasPermission("atsutilities.admin")) {
 			atsu.debug = !atsu.debug;
 			sender.sendMessage("[ATSUtilities] Debug mode set to " + atsu.debug);
+
+		} else if (args[0].equalsIgnoreCase("vampiriccocktail") && sender.hasPermission("atsutilities.admin")) {
+			
+			if (!(Bukkit.getServer().getPlayer(args[1]) instanceof Player)) {
+				return false;
+			}
+			
+			Player p = Bukkit.getServer().getPlayer(args[1]);
+			ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+			Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run particle minecraft:smoke ~ ~1 ~ 0.25 0.5 0.25 0 100");
+			Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run particle minecraft:large_smoke ~ ~1 ~ 0.25 0.5 0.25 0 20");
+			Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run playsound minecraft:entity.enderman.teleport master @a ~ ~ ~ 1 1");
+			LivingEntity entity = (LivingEntity) p.getWorld().spawnEntity(p.getLocation(), EntityType.BAT);
+			entity.setCustomName(p.getName());
+			entity.setCustomNameVisible(true);
+			entity.setRemoveWhenFarAway(false);
+			p.setGameMode(GameMode.SPECTATOR);
+			p.setSpectatorTarget(entity);
+			if(p.getSpectatorTarget() == null || !(p.getSpectatorTarget().getUniqueId().equals(entity.getUniqueId()))) {
+				p.setGameMode(GameMode.SURVIVAL);
+			}
+			Bukkit.getScheduler().runTaskLater(atsu, new Runnable() {
+				@Override
+				public void run() {
+					if (p.getGameMode().equals(GameMode.SPECTATOR)) {
+						Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run particle minecraft:smoke ~ ~1 ~ 0.25 0.5 0.25 0 100");
+						Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run particle minecraft:large_smoke ~ ~1 ~ 0.25 0.5 0.25 0 20");
+						Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run playsound minecraft:entity.enderman.teleport master @a ~ ~ ~ 1 1");
+						entity.remove();
+						p.setGameMode(GameMode.SURVIVAL);
+					}
+				}
+			}, Integer.parseInt(args[2]) * 20);
+			checkInPOV(p, entity, 0, Integer.parseInt(args[2]) * 5);
+			return true;
 
 		} else if (args[0].equalsIgnoreCase("give") && sender.hasPermission("atsutilities.admin")) {
 
@@ -61,6 +100,40 @@ public class ATSU {
 			return true;
 		}
 		return false;
+	}
+	
+	public static void checkInPOV(Player p, LivingEntity entity, int count, int countTo) {
+		
+		final int pass = count++;
+		
+		if (count >= countTo) {
+			return;
+		}
+		
+		if (!p.getGameMode().equals(GameMode.SPECTATOR)) {
+			return;
+		}
+		
+		if (entity.isDead()) {
+			p.setGameMode(GameMode.SURVIVAL);
+			ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+			Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run particle minecraft:smoke ~ ~1 ~ 0.25 0.5 0.25 0 100");
+			Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run particle minecraft:large_smoke ~ ~1 ~ 0.25 0.5 0.25 0 20");
+			Bukkit.dispatchCommand(console, "execute at " + p.getName() + " run playsound minecraft:entity.enderman.teleport master @a ~ ~ ~ 1 1");
+			entity.remove();
+			return;
+		}
+		
+		if (p.getSpectatorTarget() == null || !(p.getSpectatorTarget().getUniqueId().equals(entity.getUniqueId()))) {
+			p.setSpectatorTarget(entity);
+		}
+		
+		Bukkit.getScheduler().runTaskLater(atsu, new Runnable() {
+			@Override
+			public void run() {
+				checkInPOV(p, entity, pass, countTo);
+			}
+		}, 4);
 	}
 
 }
