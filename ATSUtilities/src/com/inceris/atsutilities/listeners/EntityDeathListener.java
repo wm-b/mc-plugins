602021acdc78460f91af6c5a1f6f235d199552bf
w-mb <william.baines@students.plymouth.ac.uk>
2022-08-29 18:17:19 +0100

    Mobs players have transformed into don't drop items.

---
+++ ATSUtilities/src/com/inceris/atsutilities/listeners/EntityDeathListener.java
@@ -0,0 +1,22 @@
+package com.inceris.atsutilities.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import com.inceris.atsutilities.ATSUtilities;
+
+public class EntityDeathListener implements Listener {
+
+	private static ATSUtilities atsu = ATSUtilities.getPlugin(ATSUtilities.class);
+
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent e) {
+		
+		if (atsu.transformMobs.contains(e.getEntity().getUniqueId())) {
+			e.getDrops().clear();
+		}
+		
+	}
+	
+}
