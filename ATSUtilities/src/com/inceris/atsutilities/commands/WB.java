f1214c767df5f81204dba763c76eb9ea99255352
w-mb <william.baines@students.plymouth.ac.uk>
2022-08-30 22:59:18 +0100

    Added swappable /wb functionality between /craft and welcoming back.

--- ATSUtilities/src/com/inceris/atsutilities/commands/WB.java
+++ ATSUtilities/src/com/inceris/atsutilities/commands/WB.java
@@ -3,6 +3,10 @@ package com.inceris.atsutilities.commands;
 import com.inceris.atsutilities.ATSUtilities;
 import com.inceris.atsutilities.util.Util;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -10,19 +14,28 @@ public class WB {
 
 	private static ATSUtilities pl = ATSUtilities.getPlugin(ATSUtilities.class);
 
-	public static boolean cmd(CommandSender sender) {
+	public static boolean cmd(CommandSender sender, String[] args) {
+		
+		if (!(sender instanceof Player)) {
+			return false;
+		}
+		
+		Player p = (Player) sender;
+		
+		if (!wbData(p, args)) {
+			return true;
+		}
+		
+		if (pl.wbData.getStringList("WorkbenchPreferred").contains(p.getUniqueId().toString())) {
+			Bukkit.dispatchCommand(sender, "craft");
+			return true;
+		}
 
 		if (pl.latestJoin == null) {
 			sender.sendMessage(Util.colours(
 					"&cNobody has joined this server recently enough to welcome! Please note, if somebody has joined the network but not the server you're on, this command will not work."));
 			return true;
 		}
-		
-		if (!(sender instanceof Player)) {
-			return true;
-		}
-		
-		Player p = (Player) sender;
 		
 		String name = pl.latestJoin;
 		
@@ -60,6 +73,49 @@ public class WB {
 
 		return true;
 
+	}
+	
+	public static boolean wbData(Player p, String[] args) {
+		
+		if (args.length < 1) {
+			return true;
+		}
+		
+		if (args[0].equalsIgnoreCase("workbench")) {
+			if (!pl.wbData.getStringList("WorkbenchPreferred").contains(p.getUniqueId().toString())) {
+				List<String> put = pl.wbData.getStringList("WorkbenchPreferred");
+				put.add(p.getUniqueId().toString());
+				pl.wbData.set("WorkbenchPreferred", put);
+				try {
+					pl.wbData.save(pl.wbDataFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				p.sendMessage(Util.colours("&8[&4Server&8] &fYour &9/wb &fpreference has been set to 'Workbench'."));
+			} else {
+				p.sendMessage(Util.colours("&8[&4Server&8] &fYour &9/wb &fpreference is already set to 'Workbench'."));
+			}
+			return false;
+		}
+		
+		if (args[0].equalsIgnoreCase("welcome")) {
+			if (pl.wbData.getStringList("WorkbenchPreferred").contains(p.getUniqueId().toString())) {
+				List<String> put = pl.wbData.getStringList("WorkbenchPreferred");
+				put.remove(p.getUniqueId().toString());
+				pl.wbData.set("WorkbenchPreferred", put);
+				try {
+					pl.wbData.save(pl.wbDataFile);
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				p.sendMessage(Util.colours("&8[&4Server&8] &fYour &9/wb &fpreference has been set to 'Welcome'."));
+			} else {
+				p.sendMessage(Util.colours("&8[&4Server&8] &fYour &9/wb &fpreference is already set to 'Welcome'."));
+			}
+			return false;
+		}
+		
+		return true;
 	}
 
 }
