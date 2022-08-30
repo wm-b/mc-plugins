5974c4e73579f6a217d8d3937cf59a29a42488de
w-mb <william.baines@students.plymouth.ac.uk>
2022-08-30 22:58:42 +0100

    Added wbData.yml file creation and loading.

--- ATSUtilities/src/com/inceris/atsutilities/ATSUtilities.java
+++ ATSUtilities/src/com/inceris/atsutilities/ATSUtilities.java
@@ -5,6 +5,7 @@ import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.inceris.atsutilities.listeners.PrepareAnvilListener;
+import com.inceris.atsutilities.tabcompletion.WbTabCompletion;
 
 import net.luckperms.api.LuckPerms;
 
@@ -29,6 +30,8 @@ import com.inceris.atsutilities.listeners.PlayerInteractListener;
 import com.inceris.atsutilities.listeners.PlayerJoinListener;
 import com.inceris.atsutilities.listeners.PlayerQuitListener;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -37,11 +40,14 @@ import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.enchantments.Enchantment;
 
 public class ATSUtilities extends JavaPlugin {
 
-	public static LuckPerms lp;
+	public YamlConfiguration wbData = null;
+	public File wbDataFile = new File("plugins/ATSUtilities/wbData.yml");
+	public static LuckPerms lp = null;
 	public String latestJoin = null;
 	public boolean denyTallGrass = false;
 	public boolean denyInfested = true;
@@ -63,7 +69,23 @@ public class ATSUtilities extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
+		
 		this.saveDefaultConfig();
+		
+        if(!wbDataFile.exists()){
+            try {
+            	wbDataFile.createNewFile();
+                wbData = YamlConfiguration.loadConfiguration(wbDataFile);
+                wbData.addDefault("WorkbenchPreferred", new ArrayList<String>());
+                wbData.options().copyDefaults(true);
+                wbData.save(wbDataFile);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+        	wbData = YamlConfiguration.loadConfiguration(wbDataFile);
+        }
+		
 		PluginManager pm = getServer().getPluginManager();
 		pm.registerEvents(new IronGolemDeathListener(), this);
 		pm.registerEvents(new DurabilityLossListener(), this);
@@ -78,10 +100,14 @@ public class ATSUtilities extends JavaPlugin {
 		pm.registerEvents(new PlayerQuitListener(), this);
 		pm.registerEvents(new EntityDeathListener(), this);
 		
+		getCommand("wb").setTabCompleter(new WbTabCompletion());
+		getCommand("welcome").setTabCompleter(new WbTabCompletion());
+		
 		RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
 		if (provider != null) {
 		    lp = provider.getProvider();
 		}
+		
 	}
 
 	@Override
@@ -122,7 +148,7 @@ public class ATSUtilities extends JavaPlugin {
 			}
 
 			if (label.equalsIgnoreCase("wb") || label.equalsIgnoreCase("welcome")) {
-				return WB.cmd(sender);
+				return WB.cmd(sender, args);
 			}
 
 			if (label.equalsIgnoreCase("countdown") || label.equalsIgnoreCase("cd")) {
